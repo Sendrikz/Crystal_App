f0619f73266b330de649f84157e2052fabb668b1
root <root@LAPTOP-89CH9ORI.localdomain>
2020-04-11 01:02:44 +0300

    Delete double check of password #2

--- src/controllers/RegisterController.cr
+++ src/controllers/RegisterController.cr
@@ -4,7 +4,6 @@ require "../models/*"
 post "/register-me" do |env|
   email = env.params.body["email"].as(String)
   password = env.params.body["password"].as(String)
-  confirm_password = env.params.body["confirm_password"].as(String)
   name = env.params.body["name"].as(String)
   surname = env.params.body["surname"].as(String)
   year_of_study = env.params.body["year_of_study"].as(String).to_i
